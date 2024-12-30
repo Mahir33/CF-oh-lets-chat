625330c69d40c411879b5f100f29d75b388ce87e
Mahir33 <adam.marek.wrobel@gmail.com>
2024-12-30 13:51:50 +0100

    Implementing firebase

--- components/chat/Chat.js
+++ components/chat/Chat.js
@@ -5,62 +5,77 @@
 // Import statements
 import { GiftedChat } from 'react-native-gifted-chat';
 import { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { 
+    collection, 
+    addDoc, 
+    onSnapshot, 
+    query, 
+    where, 
+    orderBy 
+} from "firebase/firestore";
+import { v4 as uuidv4 } from 'uuid';
 
 // Chat component
-const Chat = ({route, navigation}) => {
+const Chat = ({route, navigation, db}) => {
 
     // State variable to store the messages in the chat
     const [ messages, setMessages ] = useState([]);
 
-    // Extract the name and color from the route parameters
-    const {name, color: color} = route.params;
-    
-    // Function to handle sending messages in the chat
-    const onSend = (messages) => {
-        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
-    }
+    // Extract the name, color and userID from the Start component
+    const {name, color, userID} = route.params;
 
     useEffect(() => {
         // Set the navigation title to the name passed in the route parameters
         navigation.setOptions({title: name});
 
         // Set the initial messages in the chat
-        setMessages([
-            {
-                _id: 1,
-                text: 'Welcome to the chat!',
-                createdAt: new Date(),
-                system: true,
-            },
-            {
-                _id: 2,
-                text: 'Hello!',
-                createdAt: new Date(),
-                user: {
-                    _id: 3,
-                    name: 'Adam',
-                    avatar: 'https://placeimg.com/140/140/any',
-                },
-            },
-        ])}  
-    , []
+        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"), where ("uid", "==", userID));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            let newMessages = [];
+            querySnapshot.forEach((docObject) => {
+                newMessages.push(docObject.data());
+            });
+            setMessages(newMessages);
+        })
+        return () => {
+            if (unsubscribe) unsubscribe();  // Unsubscribe from the listener when the component unmounts
+            
+        }
+    }, []
     );
+
+    const onSend = async (newMessage) => {
+        
+        const newMessageRef = await addDoc(collection(db, "messages"), newMessage);
+        if (newMessageRef.id) {
+          setMessages([newMessage, ...messages]);
+          Alert.alert(`The message "${newMessage}" has been sent.`);
+        }else{
+          Alert.alert("Unable to add. Please try later");
+        }
+      }
 
     return (
         // Set the background color of the container to the color passed in the route parameters
-        <View style={[styles.container, {backgroundColor: color}]} >
-            {/* 
-            GiftedChat component to display the chat messages.
-             */}
-            <GiftedChat 
-                messages={messages}
-                onSend={onSend}
-                user={{
-                    _id: 1,
-                }}
-            />
-        </View>
+        <View style={[styles.container, { backgroundColor: color }]}>
+        <GiftedChat
+          messages={messages}
+          onSend={(messages = []) => {
+            const newMessage = {
+              _id: uuidv4(),
+              text: messages[0]?.text || '',
+              createdAt: new Date(),
+              user: {
+                _id: userID,
+                name: name,
+                avatar: "link"
+              }
+            };
+            onSend(newMessage);
+          }}
+        />
+      </View>
     );
 }
 
